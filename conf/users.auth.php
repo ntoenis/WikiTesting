8cf938caa2880807c7ab4bac3ac2005dc209f9bc
Nicholas Toeniskoetter <ntoenis@gmail.com>
2017-12-08 11:27:27 -0600

    Code upload
    First Commit

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+ntoenis:$1$XeNpJvCQ$ZXZIphWWwuRF.7vsfIOtj/:Nick Toeniskoetter:ntoenis@gmail.com:admin,user
